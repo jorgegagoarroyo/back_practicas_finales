27ef64dd63e2a5368536d30c9a1774d96e6d729b
jorge <gagoarroyojorge@fpllefia.com>
2022-07-27 16:50:53 +0200

    cambios en controlador generico

--- src/controladores/generico.js
+++ src/controladores/generico.js
@@ -1,17 +1,20 @@
 const db = require("../database")
 
-const tabla = "empleados"
+const tabla = "alumnos"
 
-let campos = lista_campos()
+let fields = lista_campos()
 
 async function lista_campos(){
     await db.execute(`DESCRIBE ${tabla} `, (err, resul)=>{
             if(err){
                 console.log(`al leer los campos de la base de datos de la tabla ${tabla} ocurrio un error : `,err)
-                campos = false
+                fields = false
             }
-            campos = resul
-            campos = campos.map((i)=>{return i.Field})  
+            fields = resul
+            //console.log(fields)
+            fields = fields.map((i)=>{return `{ ${ i.Field}:${i.Type}}`})
+            console.log(fields)
+          
         })
     
 }
@@ -27,14 +30,45 @@ module.exports = {
         })
     },
     campos_tabla: async (req, res)=>{
-            if(!campos){
+            if(!fields){
                 res.status(500).json({mensaje : `error al leer los campos en la tabla ${tabla}`})
             }else{
-                res.status(200).json({campos})
+                res.status(200).json({fields})
             }
     },
-    crear_elemento: (req, res)=>{
-        db.execute(`INSERT INTO ${tabla}`)
+    crear_elemento: async (req, res)=>{
+        let datos = req.body.campos
+        let temp = []
+        let query = ""
+        let values = []
+
+
+        temp = Object.keys(datos)
+
+        //console.log("temp "+ temp )
+
+        await temp.forEach(element => {
+            console.log("elemento ", element)
+            if(fields.includes(element)){
+                console.log(datos[element])
+                query += ` ${element},`
+                if(fields.element[1] != "INT" || fields.element[1] != "DATE"){
+                    values += ` '${datos[element]}',`
+                }else{
+                    values += ` ${datos[element]},`
+                }
+            }
+        })
+        query = query.slice(0, -1)
+        values = values.slice(0, -1)
+            
+        console.log("query ", query)
+        console.log("values ",values)
+        //console.log("fields ",fields)
+        res.json({mensaje:`INSERT INTO ${tabla} (${query}) VALUES (${values})`})
+
+        //cambiar el prepared statement???
+        db.execute(`INSERT INTO ${tabla} (${query}) VALUES (${values})`)
     },
     editar_elemento: (req, res)=>{
         res.send("editar elemento")
@@ -54,3 +88,35 @@ module.exports = {
     //         }
     //     })
     // },
+
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    //base para cambiar los prepared statements segun los campos pasados en la peticion y los campos de la tabla
+
+    // name: (req, res)=>{
+    //     let datos = req.body.campos
+    //     let temp = []
+    //     let query = ""
+    //     let values = []
+
+
+    //     temp = Object.keys(datos)
+
+    //     //console.log("temp "+ temp )
+
+    //     temp.forEach(element => {
+    //         //console.log("elemento ", element)
+    //         if(fields.includes(element)){
+    //             console.log(datos[element])
+    //             query += `${element}=?,`
+    //             values.push(datos[element])
+
+    //         }
+    //     })
+            
+        
+
+    //     // console.log("query ", query)
+    //     // console.log("values ",values)
+    //     // console.log("fields ",fields)
+    //     res.json({mensaje:"ok"})
